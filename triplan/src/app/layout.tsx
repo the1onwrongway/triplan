29c85616a94ca6bd2321105dbd5870477a2b002f
Milan Gabriel <mcwan33@gmail.com>
2025-08-23 12:46:04 +0530

    Added vercel analytics

--- triplan/src/app/layout.tsx
+++ triplan/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"; // For Vercel Analytics
 
 
 const geistSans = Geist({
@@ -29,6 +30,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics />
+
       </body>
     </html>
   );
